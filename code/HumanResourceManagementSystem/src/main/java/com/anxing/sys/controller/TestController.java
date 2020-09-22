6bf5fd34186a6fde480fdd445deb3d76712adeef
anxinghei <846939029@qq.com>
2020-09-22 17:57:28 +0800

    全局异常处理和数据统一响应

--- code/HumanResourceManagementSystem/src/main/java/com/anxing/sys/controller/TestController.java
+++ code/HumanResourceManagementSystem/src/main/java/com/anxing/sys/controller/TestController.java
@@ -2,8 +2,10 @@ package com.anxing.sys.controller;
 
 import javax.validation.Valid;
 
+import com.anxing.sys.exception.ResultVO;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +30,27 @@ public class TestController {
 		return "postBody id OK";
 	}
 	
-	@PostMapping("/addUser")
-    public String addUser(@RequestBody @Valid User user, BindingResult bindingResult) {
+	@PostMapping("/addUser2")
+    public String addUser2(@RequestBody @Valid User user, BindingResult bindingResult) {
         // 如果有参数校验失败，会将错误信息封装成对象组装在BindingResult里
         for (ObjectError error : bindingResult.getAllErrors()) {
             return error.getDefaultMessage();
         }
         return "OK";
     }
+	@PostMapping("/addUser")
+    public User addUser(@RequestBody @Valid User user) {
+        return user;
+    }
+
+	@GetMapping("/getUser")
+	public User getUser() {
+	    User user = new User();
+	    user.setId(1L);
+	    user.setAccount("12345678");
+	    user.setPassword("12345678");
+	    user.setEmail("123@qq.com");
+	    // 注意哦，这里是直接返回的User类型，并没有用ResultVO进行包装
+	    return user;
+	}
 }
---
+++ code/HumanResourceManagementSystem/src/main/java/com/anxing/sys/exception/APIException.java
@@ -0,0 +1,23 @@
+package com.anxing.sys.exception;
+
+import lombok.Getter;
+
+@Getter //只要getter方法，无需setter
+public class APIException extends RuntimeException {
+    private int code;
+    private String msg;
+
+    public APIException() {
+        this(1001, "接口错误");
+    }
+
+    public APIException(String msg) {
+        this(1001, msg);
+    }
+
+    public APIException(int code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+}
